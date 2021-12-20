2d1fc83d783646ac8b3c786543c317de4134b6d9
Mihail-Mihov <ebayshop@mail.bg>
2021-12-21 00:09:18 +0200

    Add comments for Session Time Interceptor

--- src/main/java/com/example/myproject/config/WebConfiguration.java
+++ src/main/java/com/example/myproject/config/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.myproject.config;
 
-import com.example.myproject.web.interceptor.StatsInterceptor;
+import com.example.myproject.web.interceptor.SessionTimerInterceptor;
+import com.example.myproject.web.interceptor.UserInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,14 +9,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    private final StatsInterceptor statsInterceptor;
+//    private final Interceptor statsInterceptor;
+//
+//    public WebConfiguration(Interceptor statsInterceptor) {
+//        this.statsInterceptor = statsInterceptor;
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(statsInterceptor);
+//    }
+//
 
-    public WebConfiguration(StatsInterceptor statsInterceptor) {
-        this.statsInterceptor = statsInterceptor;
-    }
+
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new LoggerInterceptor());
+//
+//    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(statsInterceptor);
+        registry.addInterceptor(new SessionTimerInterceptor());
+        registry.addInterceptor(new UserInterceptor());
     }
+
+
 }
